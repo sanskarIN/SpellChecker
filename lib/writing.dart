f835cfb23ae800b6d1f4e31c9a5b1ffc8128f7d0
Sanskar <sanskarin@outlook.in>
2026-08-08 15:45:08 +0530

    feat: add public local writing rules api barrel

---
+++ lib/writing.dart
@@ -0,0 +1,11 @@
+/// Local writing-rule APIs for SpellChecker 2.0 and later.
+library;
+
+export 'writing/rules/repeated_punctuation_rule.dart';
+export 'writing/rules/repeated_space_rule.dart';
+export 'writing/rules/repeated_word_rule.dart';
+export 'writing/rules/sentence_capitalization_rule.dart';
+export 'writing/writing_analyzer.dart';
+export 'writing/writing_correction.dart';
+export 'writing/writing_issue.dart';
+export 'writing/writing_rule.dart';
